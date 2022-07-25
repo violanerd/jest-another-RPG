69ffdc1a2fd93a40a558cd3fff17a31410575951
violanerd <madalyne.cross@gmail.com>
2022-07-25 17:25:18 -0500

    added player prototype functions

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -22,7 +22,7 @@ test("gets a player's stats as an object", () => {
     expect(player.getStats()).toHaveProperty('health');
     expect(player.getStats()).toHaveProperty('strength');
     expect(player.getStats()).toHaveProperty('agility');
-})
+});
 
 test('gets inventory from player or returns false', () => {
     const player = new Player('Dave');
@@ -32,4 +32,33 @@ test('gets inventory from player or returns false', () => {
     player.inventory = [];
 
     expect(player.getInventory()).toEqual(false);
-})
+});
+
+test('gets a players health value', () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test('subtracts from players health', () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
