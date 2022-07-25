12ec4173674ffe6125d78ec4cf26c1ff3c7fbf6a
violanerd <madalyne.cross@gmail.com>
2022-07-25 18:34:40 -0500

    adding player functions

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -62,3 +62,11 @@ test('subtracts from players health', () => {
 
     expect(player.health).toBe(0);
 });
+
+test('gets players attack value', () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
